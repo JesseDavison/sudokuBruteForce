0e7743ccc21266ecefeb6fc8f472ccc6dfa9b45c
JesseDavison <jessedavison@gmail.com>
2022-04-11 22:42:37 -0700

    cleaned up unused lines, howToUse is at bottom

--- sudokuBruteForce.py
+++ sudokuBruteForce.py
@@ -21,12 +21,6 @@ def printTable(table):
             print("-------------------------")
 
 
-def createTable(table):
-    blankTable = [['-' for j in range(9)] for i in range(9)]
-    for row in range(9):
-        print('row ' + str(row))
-        for column in range(9):
-            blankTable[row][column] = input('column ' + str(column) + ': ')
 
 
 def willFit(table, int, row, column):
@@ -120,134 +114,18 @@ def solveBrutely(table):
 
 
 
-
-
-
-
-
-
-
-
-
-sudokuTable = [[['-', False] for j in range(9)] for i in range(9)]          # False that it is hardcoded... so it can be changed
-
-sudokuTable[0][0] = [7, True]           # True that it is hardcoded, and cannot be changed
-# sudokuTable[0][1] = [1, True]
-sudokuTable[0][2] = [2, True]
-sudokuTable[0][3] = [9, True]
-sudokuTable[0][4] = [1, True]
-sudokuTable[0][5] = [4, True]
-sudokuTable[0][6] = [8, True]
-sudokuTable[0][7] = [5, True]
-# sudokuTable[0][8] = [7, True]
-
-sudokuTable[1][0] = [3, True]
-# sudokuTable[1][1] = [1, True]
-sudokuTable[1][2] = [4, True]
-# sudokuTable[1][3] = [1, True]
-sudokuTable[1][4] = [7, True]
-sudokuTable[1][5] = [8, True]
-sudokuTable[1][6] = [2, True]
-sudokuTable[1][7] = [6, True]
-sudokuTable[1][8] = [9, True]
-
-sudokuTable[2][0] = [5, True]
-# sudokuTable[2][1] = [1, True]
-sudokuTable[2][2] = [9, True]
-# sudokuTable[2][3] = [1, True]
-# sudokuTable[2][4] = [8, True]
-# sudokuTable[2][5] = [3, True]
-# sudokuTable[2][6] = [5, True]
-# sudokuTable[2][7] = [6, True]
-sudokuTable[2][8] = [1, True]
-
-# sudokuTable[3][0] = [1, True]
-# sudokuTable[3][1] = [1, True]
-sudokuTable[3][2] = [7, True]
-# sudokuTable[3][3] = [1, True]
-# sudokuTable[3][4] = [8, True]
-# sudokuTable[3][5] = [3, True]
-sudokuTable[3][6] = [3, True]
-sudokuTable[3][7] = [8, True]
-# sudokuTable[3][8] = [1, True]
-
-# sudokuTable[4][0] = [1, True]
-# sudokuTable[4][1] = [1, True]
-# sudokuTable[4][2] = [1, True]
-sudokuTable[4][3] = [8, True]
-# sudokuTable[4][4] = [8, True]
-sudokuTable[4][5] = [1, True]
-sudokuTable[4][6] = [6, True]
-# sudokuTable[4][7] = [6, True]
-sudokuTable[4][8] = [7, True]
-
-# sudokuTable[5][0] = [1, True]
-sudokuTable[5][1] = [9, True]
-# sudokuTable[5][2] = [1, True]
-# sudokuTable[5][3] = [1, True]
-sudokuTable[5][4] = [3, True]
-# sudokuTable[5][5] = [3, True]
-# sudokuTable[5][6] = [5, True]
-# sudokuTable[5][7] = [6, True]
-# sudokuTable[5][8] = [1, True]
-
-# sudokuTable[6][0] = [1, True]
-sudokuTable[6][1] = [2, True]
-sudokuTable[6][2] = [1, True]
-# sudokuTable[6][3] = [1, True]
-sudokuTable[6][4] = [8, True]
-sudokuTable[6][5] = [5, True]
-# sudokuTable[6][6] = [5, True]
-# sudokuTable[6][7] = [6, True]
-# sudokuTable[6][8] = [1, True]
-
-# sudokuTable[7][0] = [1, True]
-# sudokuTable[7][1] = [1, True]
-sudokuTable[7][2] = [8, True]
-# sudokuTable[7][3] = [1, True]
-# sudokuTable[7][4] = [8, True]
-# sudokuTable[7][5] = [3, True]
-# sudokuTable[7][6] = [5, True]
-# sudokuTable[7][7] = [6, True]
-sudokuTable[7][8] = [6, True]
-
-# sudokuTable[8][0] = [1, True]
-sudokuTable[8][1] = [7, True]
-# sudokuTable[8][2] = [1, True]
-sudokuTable[8][3] = [3, True]
-# sudokuTable[8][4] = [8, True]
-sudokuTable[8][5] = [9, True]
-# sudokuTable[8][6] = [5, True]
-sudokuTable[8][7] = [2, True]
-# sudokuTable[8][8] = [1, True]
-
-
-
-
-###################################################
-# tableSolved = False
-# printTable(sudokuTable)
-# print("beginning to solve now")
-# start = datetime.now()
-# solveBrutely(sudokuTable)
-# end = datetime.now()
-# print("time passed: " + str(end - start))
-###################################################
-
-
-# willFit(sudokuTable, 9, 7, 7)
+#######################################################################
+####        enter a new Sudoku puzzle as a string
+#######################################################################
 # inputString = '--581-6---9------2-----3---5------8---867-1-----4-------7--4---2--73---6-----53--'
-# inputString = '- - - - 1 - - - 6 2- - 5 - 6 3 - - - 5 - - 4 - - - - - 2 - 7 - 9 - - 4 3 - - - - - - 8 - - - - - 5 - - - - - - - - - 1 - - - - 7 - 6 - 2 - - 9 - - 9 - - - 4 - - '
+# inputString = '- - - - 1 - - - 6 2- - 5 - 6 3 - - - 5 - - 4 - - - - - 2 - 7 - 9 - - 4 3 - - - - - - 8 - - - - - 5 - - - - - - - - - 1 - - - - 7 - 6 - 2 - - 9 - - 9 - - - 4 - - '       # takes >50sec to solve
 # inputString = '-5-17---4--6---5------2------891--7------6--9-1---2----6-45---73------8------9---'
 # inputString = '-------2---68--3-7-1--9------5----4-9---8-5-2---2---6-6----3-----37--8-5------4--'
-# inputString = '8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--'   # hardest ever
-inputString = '--53-----8------2--7--1-5--4----53---1--7---6--32---8--6-5----9--4----3------97--'
+inputString = '8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--'   # "hardest ever,"" takes 7 seconds to solve
+# inputString = '--53-----8------2--7--1-5--4----53---1--7---6--32---8--6-5----9--4----3------97--'
 inputString = inputString.replace(" ", "")
 
-
 newTable = [[['-', False] for j in range(9)] for i in range(9)]
-
-
 
 increment = 0
 for i in range(9):
@@ -261,7 +139,9 @@ for i in range(9):
 
 
 
-###################################################
+#####################################################################
+####        just create a new inputString above, then run the code
+#####################################################################
 print("printing newTable: ")
 printTable(newTable)
 tableSolved = False
